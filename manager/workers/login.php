295c40707ba3309da2d25c6c69d2d00d7541059d
Taufik Nurrohman <t.nurrohman77@gmail.com>
2014-06-10 20:35:02 +0700

    Update Login Form Layout

--- manager/workers/login.php
+++ manager/workers/login.php
@@ -1,15 +1,15 @@
 <?php echo Notify::read(); ?>
 <form class="form-login" action="<?php echo $config->url_current; ?>" method="post">
   <input name="token" type="hidden" value="<?php echo Guardian::makeToken(); ?>">
-  <label class="grid-group no-gap">
-    <span class="grid span-2 form-label"><span><?php echo $speak->username; ?></span></span>
+  <label class="grid-group">
+    <span class="grid span-2 form-label"><?php echo $speak->username; ?></span>
     <span class="grid span-4"><input name="username" type="text" autocomplete="off" autofocus></span>
   </label>
-  <label class="grid-group no-gap">
-    <span class="grid span-2 form-label"><span><?php echo $speak->password; ?></span></span>
+  <label class="grid-group">
+    <span class="grid span-2 form-label"><?php echo $speak->password; ?></span>
     <span class="grid span-4"><input name="password" type="password" autocomplete="off"></span>
   </label>
-  <div class="grid-group no-gap">
+  <div class="grid-group">
     <span class="grid span-2"></span>
     <span class="grid span-4"><button class="btn btn-primary btn-login" type="submit"><i class="fa fa-key"></i> <?php echo $speak->login; ?></button></span>
   </div>
