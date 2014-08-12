33e3de2ccb4f38ad1e2dc3280bc2e5df8ff198ea
Taufik Nurrohman <t.nurrohman77@gmail.com>
2014-08-12 19:16:58 +0700

    Dynamic Custom Fields
    Allow plugin to add custom fields in post editor dynamically as well as
    shield.

--- manager/workers/unit.composer.fields.php
+++ manager/workers/unit.composer.fields.php
@@ -2,9 +2,44 @@
 
 $fields = File::exist(STATE . DS . 'fields.txt') ? File::open(STATE . DS . 'fields.txt')->unserialize() : array();
 
+
+/**
+ * Allow shield to add custom fields dynamically by creating
+ * a file called `fields.php` saved inside a folder named as `workers`.
+ * This file contains array of fields data.
+ *
+ * -- EXAMPLE CONTENT OF `fields.php`: --------------------------------
+ *
+ *    return array(
+ *        'break_title_text' => array(
+ *            'title' => 'Break Title Text?',
+ *            'type' => 'boolean',
+ *            'value' => "",
+ *            'scope' => 'article'
+ *        )
+ *    );
+ *
+ * --------------------------------------------------------------------
+ *
+ */
+
 if($e = File::exist(SHIELD . DS . $config->shield . DS . 'workers' . DS . 'fields.php')) {
     $extra_fields = include $e;
     $fields = $fields + $extra_fields;
+}
+
+
+/**
+ * Allow plugin to add custom fields dynamically by creating
+ * a file called `fields.php` saved inside a folder named as `workers`.
+ * This file contains array of fields data.
+ */
+
+foreach(glob(PLUGIN . DS . '*', GLOB_ONLYDIR) as $folder) {
+    if(File::exist($folder . DS . 'launch.php') && $e = File::exist($folder . DS . 'workers' . DS . 'fields.php')) {
+        $extra_fields = include $e;
+        $fields = $fields + $extra_fields;
+    }
 }
 
 Weapon::fire('unit_composer_fields_before', array($FT, $fields));
@@ -24,32 +59,32 @@ if( ! empty($fields)) {
         }
         if($value['type'] == 'text' && $value['scope'] == $FT) {
             $html .= '<label class="grid-group">';
-            $html .= '<span class="grid span-1 form-label">' . $value['title'] . '</span>';
-            $html .= '<span class="grid span-5">';
+            $html .= '<span class="grid span-2 form-label">' . $value['title'] . '</span>';
+            $html .= '<span class="grid span-4">';
             $html .= '<input name="fields[' . $key . '][value]" type="text" class="input-block" value="' . (isset($field[$key]) ? Text::parse($field[$key])->to_encoded_html : $value['value']) . '">';
             $html .= '</span>';
             $html .= '</label>';
         }
         if($value['type'] == 'summary' && $value['scope'] == $FT) {
             $html .= '<label class="grid-group">';
-            $html .= '<span class="grid span-1 form-label">' . $value['title'] . '</span>';
-            $html .= '<span class="grid span-5">';
+            $html .= '<span class="grid span-2 form-label">' . $value['title'] . '</span>';
+            $html .= '<span class="grid span-4">';
             $html .= '<textarea name="fields[' . $key . '][value]" class="textarea-block">' . (isset($field[$key]) ? Text::parse($field[$key])->to_encoded_html : $value['value']) . '</textarea>';
             $html .= '</span>';
             $html .= '</label>';
         }
         if($value['type'] == 'boolean' && $value['scope'] == $FT) {
             $html .= '<div class="grid-group">';
-            $html .= '<span class="grid span-1"></span>';
-            $html .= '<span class="grid span-5">';
+            $html .= '<span class="grid span-2"></span>';
+            $html .= '<span class="grid span-4">';
             $html .= '<label><input name="fields[' . $key . '][value]" type="checkbox"' . ( ! empty($value['value']) ? ' value="' . $value['value'] . '"' : "") . (isset($field[$key]) && ! empty($field[$key]) ? ' checked' : "") . '> <span>' . $value['title'] . '</span></label>';
             $html .= '</span>';
             $html .= '</div>';
         }
         if($value['type'] == 'option' && $value['scope'] == $FT) {
             $html .= '<label class="grid-group">';
-            $html .= '<span class="grid span-1 form-label">' . $value['title'] . '</span>';
-            $html .= '<span class="grid span-5">';
+            $html .= '<span class="grid span-2 form-label">' . $value['title'] . '</span>';
+            $html .= '<span class="grid span-4">';
             $html .= '<select name="fields[' . $key . '][value]" class="select-block">';
             foreach(explode("\n", $value['value']) as $v) {
                 $v = trim($v);
