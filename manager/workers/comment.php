ca50bcfb558ada49d507d643e8b2e1019d3e214f
Taufik Nurrohman <t.nurrohman77@gmail.com>
2014-08-14 11:46:43 +0700

    Prevent Conflict with FrontEnd Comment’s CSS

--- manager/workers/comment.php
+++ manager/workers/comment.php
@@ -1,21 +1,21 @@
 <?php echo $messages; ?>
 <?php if($responses): ?>
-<ol class="comment-list">
+<ol class="response-list">
   <?php foreach($responses as $response): ?>
-  <li class="comment" id="comment-<?php echo $response->id; ?>">
-    <div class="comment-header">
+  <li class="response" id="response-<?php echo $response->id; ?>">
+    <div class="response-header">
       <?php if($response->url != '#'): ?>
-      <a class="comment-name" href="<?php echo $response->url; ?>" rel="nofollow" target="_blank"><?php echo $response->name; ?></a>
+      <a class="response-title" href="<?php echo $response->url; ?>" rel="nofollow" target="_blank"><?php echo $response->name; ?></a>
       <?php else: ?>
-      <span class="comment-name"><?php echo $response->name; ?></span>
+      <span class="response-title"><?php echo $response->name; ?></span>
       <?php endif; ?>
-      <span class="comment-time">
+      <span class="response-time">
         <time datetime="<?php echo $response->date->W3C; ?>"><?php echo Date::format($response->time, 'Y/m/d H:i:s'); ?></time>
         <a href="<?php echo $response->permalink; ?>" title="<?php echo $speak->permalink; ?>" rel="nofollow" target="_blank">#</a>
       </span>
     </div>
-    <div class="comment-body"><?php echo $response->message; ?></div>
-    <div class="comment-footer">
+    <div class="response-body"><?php echo $response->message; ?></div>
+    <div class="response-footer">
       <?php Weapon::fire('comment_footer', array($response, $article = false)); ?>
     </div>
   </li>
