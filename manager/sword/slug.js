85410b70f75da95dfc35e4ce0ebb0f939f04d8b4
Taufik Nurrohman <t.nurrohman77@gmail.com>
2014-08-31 07:29:27 +0700

    Better Slug Generator
    https://github.com/kohana/core/blob/3.3/master/utf8/transliterate_to_ascii.php

--- manager/sword/slug.js
+++ manager/sword/slug.js
@@ -9,11 +9,21 @@
 
         input.off("keyup").on("keyup", function() {
 
+            var value = this.value;
+
+            // Remove accents ...
+            var from = 'àôďḟëšơßăřțňāķŝỳņĺħṗóúěéçẁċõṡøģŧșėĉśîűćęŵṫūčöèŷąłųůşğļƒžẃḃåìïḋťŗäíŕêüòēñńĥĝđĵÿũŭưţýőâľẅżīãġṁōĩùįźáûþðæµĕıÀÔĎḞËŠƠĂŘȚŇĀĶĔŜỲŅĹĦṖÓÚĚÉÇẀĊÕṠØĢŦȘĖĈŚÎŰĆĘŴṪŪČÖÈŶĄŁŲŮŞĞĻƑŽẂḂÅÌÏḊŤŖÄÍŔÊÜÒĒÑŃĤĜĐĴŸŨŬƯŢÝŐÂĽẄŻĪÃĠṀŌĨÙĮŹÁÛÞÐÆİ',
+                to = 'aodfesossartnaksynlhpoueecwcosogtsecsiucewtucoeyaluusglfzwbaiidtraireuoennhgdjyuuutyoalwziagmoiuizauthdhaeueiAODFESOARTNAKESYNLHPOUEECWCOSOGTSECSIUCEWTUCOEYALUUSGLFZWBAIIDTRAIREUOENNHGDJYUUUTYOALWZIAGMOIUIZAUThDhAeI';
+
+            for (var i = 0, len = from.length; i < len; ++i) {
+                value = value.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+            }
+
             output.val(
-                this.value
+                value
                     .replace(/<.*?>/g, "")
-                    .replace(/[^a-z0-9-]+/gi, "-")
-                    .replace(/\-+/g, "-")
+                    .replace(/[^a-z0-9-]+/gi, '-')
+                    .replace(/\-+/g, '-')
                     .replace(/^\-|\-$/g, "")
                     .toLowerCase()
                     .replace(/\-/g, connector)
