2056da872745be1b580a3e03c09e5bd461c46f2a
Taufik Nurrohman <t.nurrohman77@gmail.com>
2014-09-02 20:23:48 +0700

    Update Facebook Open Graph Plugin

--- cabinet/plugins/facebook-open-graph/launch.php
+++ cabinet/plugins/facebook-open-graph/launch.php
@@ -4,7 +4,7 @@ Weapon::add('meta', function() {
     $config = Config::get();
     echo '<!-- Start Facebook Open Graph -->';
     echo '<meta property="og:title" content="' . strip_tags($config->page_title) . '"' . ES;
-    echo '<meta property="og:type" content="website"' . ES;
+    echo '<meta property="og:type" content="' . ($config->page_type == 'article' ? 'article' : 'website') . '"' . ES;
     echo '<meta property="og:url" content="' . $config->url_current . '"' . ES;
     if(isset($config->article->image)) {
         echo '<meta property="og:image" content="' . $config->article->image . '"' . ES;
@@ -13,11 +13,11 @@ Weapon::add('meta', function() {
     }
     echo '<meta property="og:site_name" content="' . $config->title . '"' . ES;
     if(isset($config->article->description)) {
-        echo '<meta property="og:description" content="' . Text::parse($config->article->description)->to_encoded_html . '"' . ES;
+        echo '<meta property="og:description" content="' . strip_tags($config->article->description) . '"' . ES;
     } elseif(isset($config->page->description)) {
-        echo '<meta property="og:description" content="' . Text::parse($config->page->description)->to_encoded_html . '"' . ES;
+        echo '<meta property="og:description" content="' . strip_tags($config->page->description) . '"' . ES;
     } else {
-        echo '<meta property="og:description" content="' . Text::parse($config->description)->to_encoded_html . '"' . ES;
+        echo '<meta property="og:description" content="' . strip_tags($config->description) . '"' . ES;
     }
     echo '<!-- End Facebook Open Graph -->';
 }, 11);
