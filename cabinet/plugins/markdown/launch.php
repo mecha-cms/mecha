0b4d752fa8a18ded32e0d49f82f0ad83736c5896
Taufik Nurrohman <t.nurrohman77@gmail.com>
2015-05-17 17:27:51 +0700

    Update

--- cabinet/plugins/markdown/launch.php
+++ cabinet/plugins/markdown/launch.php
@@ -2,10 +2,11 @@
 
 use \Michelf\MarkdownExtra;
 
+include PLUGIN . DS . basename(__DIR__) . DS . 'Michelf' . DS . 'Markdown.php';
+include PLUGIN . DS . basename(__DIR__) . DS . 'Michelf' . DS . 'MarkdownExtra.php';
+
 Text::parser('to_html', function($input) {
     if( ! is_string($input)) return $input;
-    include PLUGIN . DS . basename(__DIR__) . DS . 'Michelf' . DS . 'Markdown.php';
-    include PLUGIN . DS . basename(__DIR__) . DS . 'Michelf' . DS . 'MarkdownExtra.php';
     $parser = new MarkdownExtra;
     $parser->empty_element_suffix = ES;
     $parser->table_align_class_tmpl = 'text-%%'; // Define table alignment class, example: `<td class="text-right">`
