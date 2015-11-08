e06cc255af09e1da311f50c22e16529acea320f2
Taufik Nurrohman <t.nurrohman77@gmail.com>
2015-11-08 21:34:42 +0700

    < 1.2.0

--- manager/workers/cargo.php
+++ manager/workers/cargo.php
@@ -2,13 +2,16 @@
 
 Weapon::add('shield_before', function() {
     ob_start();
+    $cargo = DECK . DS . 'workers' . DS . Config::get('cargo');
     // No buffer for backend cargo, so `chunk:input` and `chunk:output` filter(s) won't work here
-    Shield::chunk(DECK . DS . 'workers' . DS . Config::get('cargo'), false, false);
+    Shield::chunk($cargo, false, false);
     $content = ob_get_clean();
-    Shield::lot('page', (object) array(
+    $o = array(
         'title' => Config::get('page_title'),
         'url' => "",
         'link' => "",
         'content' => $content
-    ));
+    );
+    Config::set('page', array_merge($o, array('cargo' => $cargo))); // < 1.2.0
+    Shield::lot('page', (object) $o);
 });
