727477aa0e6156eed4990e1a0749e72b058cc197
Taufik Nurrohman <t.nurrohman77@gmail.com>
2015-11-30 21:52:51 +0700

    String Input Treated as Form Element Name

--- lot/plugins/manager/assets/sword/slug.js
+++ lot/plugins/manager/assets/sword/slug.js
@@ -5,6 +5,12 @@
 
 (function($, base) {
     $.slug = function(input, output, connect) {
+        if (typeof input === "string") {
+            input = $('[name="' + input + '"]');
+        }
+        if (typeof output === "string") {
+            output = $('[name="' + output + '"]');
+        }
         input.off("keyup change").on("keyup change", function() {
             output.val(base.task.slug(this.value.toLowerCase(), connect));
         });
