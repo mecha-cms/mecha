b3fb1dcf80060fa77dfacd8564486cba1f24725c
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-01-16 22:04:38 +0700

    Backend Specific Plugin
    Rename `editor` with `__editor` and `editor-button` with
    `__editor-button`.

--- lot/plugins/markdown/__launch.php
+++ lot/plugins/markdown/__launch.php
@@ -29,11 +29,11 @@ Weapon::add('shield_before', function() use($speak) {
         }, 11);
         Filter::add('asset:path', function($path) {
             // Replace `HTE` with `MTE`
-            if($path === PLUGIN . DS . 'editor' . DS . 'assets' . DS . 'sword' . DS . 'hte.min.js') {
+            if($path === PLUGIN . DS . '__editor' . DS . 'assets' . DS . 'sword' . DS . 'hte.min.js') {
                 return __DIR__ . DS . 'assets' . DS . 'sword' . DS . 'mte.min.js';
             }
             // Replace default `table` button with the new one
-            if($path === PLUGIN . DS . 'editor-button' . DS . 'assets' . DS . 'sword' . DS . 'table.js') {
+            if($path === PLUGIN . DS . '__editor-button' . DS . 'assets' . DS . 'sword' . DS . 'table.js') {
                 return __DIR__ . DS . 'assets' . DS . 'sword' . DS . 'table.js';
             }
             return $path;
