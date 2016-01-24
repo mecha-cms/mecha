53f09db2191d1e22abbbe1251d54579611f60d97
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-01-24 11:03:39 +0700

    Do Not use `label` Element
    https://github.com/mecha-cms/mecha-cms/commit/ad984caad7e07feea7156f7fcc040f74ac07f1a6

--- lot/extends/chunk/comment.form.message.php
+++ lot/extends/chunk/comment.form.message.php
@@ -1,4 +1,5 @@
-<label class="grid-group">
-  <span class="grid span-1 form-label"><?php echo $speak->message; ?></span>
-  <span class="grid span-5"><?php echo Form::textarea('message', Guardian::wayback('message'), null, array('class' => 'textarea-block')); ?></span>
-</label>
+<div class="grid-group">
+  <?php $_ = 'unit:' . time(); ?>
+  <label class="grid span-1 form-label" for="<?php echo $_; ?>"><?php echo $speak->message; ?></label>
+  <span class="grid span-5"><?php echo Form::textarea('message', Guardian::wayback('message'), null, array('class' => 'textarea-block', 'id' => $_)); ?></span>
+</div>
