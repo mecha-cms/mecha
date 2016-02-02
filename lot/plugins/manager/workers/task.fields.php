b183dae2ca05a8482970947666a64b72dfc1f904
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-02-02 22:07:16 +0700

    Check `type` First

--- lot/plugins/manager/workers/task.fields.php
+++ lot/plugins/manager/workers/task.fields.php
@@ -3,7 +3,7 @@
 foreach($field as $k => $v) {
     $f = $v['type'] === 'file' || $v['type'] === 'f';
     // Remove asset field value and data
-    if(isset($v['remove']) && $f) {
+    if($f && isset($v['remove'])) {
         File::open(SUBSTANCE . DS . $v['remove'])->delete();
         Weapon::fire(array('on_substance_update', 'on_substance_destruct'), array($G, $P));
         Notify::success(Config::speak('notify_file_deleted', '<code>' . $v['remove'] . '</code>'));
@@ -14,7 +14,7 @@ foreach($field as $k => $v) {
         unset($field[$k]);
     } else {
         $e = File::E($v['value']);
-        if( ! file_exists(SUBSTANCE . DS . $e . DS . $v['value']) && $f) {
+        if($f && ! file_exists(SUBSTANCE . DS . $e . DS . $v['value'])) {
             unset($field[$k]);
         } else {
             $field[$k] = $v['value'];
