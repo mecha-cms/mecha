df635f296b2d6fd11fc3eefe70423ccf3b93ccca
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-05-07 20:17:20 +0700

    ditto

--- engine/kernel/mecha.php
+++ engine/kernel/mecha.php
@@ -15,8 +15,14 @@
 
 class Mecha extends __ {
 
-    protected static $stomach = array();
-    protected static $index = 0;
+    protected $stomach = array();
+    protected $i = 0;
+
+    public function __construct($array) {
+        $this->stomach = $array;
+        $this->i = 0;
+        return $this;
+    }
 
     // Prevent `$e` exceeds the value of `$min` and `$max`
     public static function edge($e, $min = 0, $max = 9999) {
@@ -84,9 +90,7 @@ class Mecha extends __ {
 
     // Initialize with eating
     public static function eat($array) {
-        self::$stomach = $array;
-        self::$index = 0;
-        return new static;
+        return new Mecha($array);
     }
 
     // Walk through the array
@@ -102,24 +106,24 @@ class Mecha extends __ {
     }
 
     // Check if `$array` contain `$key` -- should be faster than `in_array($key, $array)`
-    public static function has($key, $x = "\x1A") {
-        $s = is_array(self::$stomach) ? implode($x, self::$stomach) : (string) self::$stomach;
+    public function has($key, $x = "\x1A") {
+        $s = is_array($this->stomach) ? implode($x, $this->stomach) : (string) $this->stomach;
         return strpos($x . $s . $x, $x . $key . $x) !== false;
     }
 
     // Sort array based on its value's key
-    public static function order($order = 'ASC', $key = null, $preserve_key = false, $default = "\x1A") {
+    public function order($order = 'ASC', $key = null, $preserve_key = false, $default = "\x1A") {
         if( ! is_null($key)) {
             $before = array();
             $after = array();
-            if(self::$stomach && ! empty(self::$stomach)) {
-                foreach(self::$stomach as $k => $v) {
+            if( ! empty($this->stomach)) {
+                foreach($this->stomach as $k => $v) {
                     $v = (array) $v;
                     if(array_key_exists($key, $v)) {
                         $before[$k] = $v[$key];
                     } else if($default !== "\x1A") {
                         $before[$k] = $default;
-                        self::$stomach[$k][$key] = $default;
+                        $this->stomach[$k][$key] = $default;
                     }
                 }
                 if($order === 'DESC') {
@@ -128,137 +132,149 @@ class Mecha extends __ {
                     asort($before);
                 }
                 foreach($before as $k => $v) {
-                    $after[$k] = self::$stomach[$k];
+                    $after[$k] = $this->stomach[$k];
                 }
             }
-            self::$stomach = $after;
+            $this->stomach = $after;
             unset($before, $after);
         } else {
-            self::$stomach = (array) self::$stomach;
+            $this->stomach = (array) $this->stomach;
             if($order === 'DESC') {
-                arsort(self::$stomach);
+                arsort($this->stomach);
             } else {
-                asort(self::$stomach);
+                asort($this->stomach);
             }
         }
         if( ! $preserve_key) {
-            self::$stomach = array_values(self::$stomach);
+            $this->stomach = array_values($this->stomach);
         }
-        return new static;
+        return $this;
     }
 
     // Array shake
-    public static function shake() {
-        shuffle(self::$stomach);
-        return new static;
+    public function shake() {
+        shuffle($this->stomach);
+        return $this;
     }
 
     // Vomit! BLARGH!
-    public static function vomit($param = null, $fallback = false) {
-        return self::GVR(self::$stomach, $param, $fallback);
+    public function vomit($param = null, $fallback = false) {
+        return self::GVR($this->stomach, $param, $fallback);
     }
 
     // Move to next array index
-    public static function next($skip = 0) {
-        self::$index = self::edge(self::$index + 1 + $skip, 0, self::count() - 1);
-        return new static;
+    public function next($skip = 0) {
+        $this->i = self::edge($this->i + 1 + $skip, 0, $this->count() - 1);
+        return $this;
     }
 
     // Move to previous array index
-    public static function prev($skip = 0) {
-        self::$index = self::edge(self::$index - 1 - $skip, 0, self::count() - 1);
-        return new static;
+    public function prev($skip = 0) {
+        $this->i = self::edge($this->i - 1 - $skip, 0, $this->count() - 1);
+        return $this;
     }
 
     // Move to `$index` array index
-    public static function to($index) {
-        self::$index = is_int($index) ? $index : self::index($index, $index);
-        return new static;
+    public function to($index) {
+        $this->i = is_int($index) ? $index : self::index($index, $index);
+        return $this;
     }
 
     // Insert `$food` before current array index
-    public static function before($food, $key = null) {
-        if(is_null($key)) $key = self::$index;
-        self::$stomach = array_slice(self::$stomach, 0, self::$index, true) + array($key => $food) + array_slice(self::$stomach, self::$index, null, true);
-        self::$index = self::edge(self::$index - 1, 0, self::count() - 1);
-        return new static;
+    public function before($food, $key = null) {
+        if(is_null($key)) $key = $this->i;
+        $this->stomach = array_slice($this->stomach, 0, $this->i, true) + array($key => $food) + array_slice($this->stomach, $this->i, null, true);
+        $this->i = self::edge($this->i - 1, 0, $this->count() - 1);
+        return $this;
     }
 
     // Insert `$food` after current array index
-    public static function after($food, $key = null) {
-        if(is_null($key)) $key = self::$index + 1;
-        self::$stomach = array_slice(self::$stomach, 0, self::$index + 1, true) + array($key => $food) + array_slice(self::$stomach, self::$index + 1, null, true);
-        self::$index = self::edge(self::$index + 1, 0, self::count() - 1);
-        return new static;
+    public function after($food, $key = null) {
+        if(is_null($key)) $key = $this->i + 1;
+        $this->stomach = array_slice($this->stomach, 0, $this->i + 1, true) + array($key => $food) + array_slice($this->stomach, $this->i + 1, null, true);
+        $this->i = self::edge($this->i + 1, 0, $this->count() - 1);
+        return $this;
     }
 
     // Replace current array index value with `$food`
-    public static function replace($food) {
+    public function replace($food) {
         $i = 0;
-        foreach(self::$stomach as $k => $v) {
-            if($i === self::$index) {
-                self::$stomach[$k] = $food;
+        foreach($this->stomach as $k => $v) {
+            if($i === $this->i) {
+                $this->stomach[$k] = $food;
                 break;
             }
             $i++;
         }
-        return new static;
+        return $this;
     }
 
     // Append `$food` to array
-    public static function append($food, $key = null) {
-        self::$index = self::count() - 1;
-        return self::after($food, $key);
+    public function append($food, $key = null) {
+        $this->i = $this->count() - 1;
+        return $this->after($food, $key);
     }
 
     // Prepend `$food` to array
-    public static function prepend($food, $key = null) {
-        self::$index = 0;
-        return self::before($food, $key);
+    public function prepend($food, $key = null) {
+        $this->i = 0;
+        return $this->before($food, $key);
     }
 
     // Get first array value
-    public static function first() {
-        self::$index = 0;
-        return reset(self::$stomach);
+    public function first() {
+        $this->i = 0;
+        return reset($this->stomach);
     }
 
     // Get last array value
-    public static function last() {
-        self::$index = self::count() - 1;
-        return end(self::$stomach);
+    public function last() {
+        $this->i = $this->count() - 1;
+        return end($this->stomach);
     }
 
     // Get current array index
-    public static function current() {
-        return self::$index;
+    public function current() {
+        return $this->i;
     }
 
     // Get selected array value
-    public static function get($index = null, $fallback = false) {
+    public function get($index = null, $fallback = false) {
         if( ! is_null($index)) {
             if(is_int($index)) {
-                $index = self::key($index, $index);
+                $index = $this->key($index, $index);
             }
-            return array_key_exists($index, self::$stomach) ? self::$stomach[$index] : $fallback;
+            return array_key_exists($index, $this->stomach) ? $this->stomach[$index] : $fallback;
         }
         $i = 0;
-        foreach(self::$stomach as $k => $v) {
-            if($i === self::$index) {
-                return self::$stomach[$k];
+        foreach($this->stomach as $k => $v) {
+            if($i === $this->i) {
+                return $this->stomach[$k];
             }
             $i++;
         }
     }
 
     // Get array length
-    public static function count($data = null) {
-        return is_null($data) ? count(self::$stomach) : count($data);
+    public function count() {
+        return count($this->stomach);
+    }
+
+    // Get array key by position
+    public function key($index, $fallback = false) {
+        $array = array_keys($this->stomach);
+        return isset($array[$index]) ? $array[$index] : $fallback;
+    }
+
+    // Get position by array key
+    public function index($key, $fallback = false) {
+        $key = array_search($key, array_keys($this->stomach));
+        return $key !== false ? $key : $fallback;
     }
 
     // Generate chunk(s) of array
-    public static function chunk($index = null, $count = 25) {
-        if( ! is_array(self::$stomach)) return new static;
+    public function chunk($index = null, $count = 25) {
+        if( ! is_array($this->stomach)) return $this;
         $results = array();
         // 0-based index with `vomit($index)`
         // `Mecha::eat($foo)->chunk(25)->vomit(1);`
@@ -266,35 +282,23 @@ class Mecha extends __ {
             $count = $index;
             $index = null;
         }
-        $chunk = array_chunk(self::$stomach, $count, true);
+        $chunk = array_chunk($this->stomach, $count, true);
         // 1-based index with `chunk($index)`
         // `Mecha::eat($foo)->chunk(2, 25)->vomit();`
         if( ! is_null($index)) {
             $chunk = isset($chunk[$index - 1]) ? $chunk[$index - 1] : false;
-            self::$stomach = $chunk ? array_values($chunk) : array();
+            $this->stomach = $chunk ? array_values($chunk) : array();
         // `Mecha::eat($foo)->chunk(null, 25)->vomit()`
         } else {
-            self::$stomach = $chunk;
+            $this->stomach = $chunk;
         }
-        return new static;
+        return $this;
     }
 
     // Shortcut for string-based `switch` and `case`
     public static function alter($case, $cases, $default = null) {
         if(is_null($default)) $default = $case;
         return array_key_exists($case, $cases) ? $cases[$case] : $default;
-    }
-
-    // Get array key by position
-    public static function key($index, $fallback = false) {
-        $array = array_keys(self::$stomach);
-        return isset($array[$index]) ? $array[$index] : $fallback;
-    }
-
-    // Get position by array key
-    public static function index($key, $fallback = false) {
-        $key = array_search($key, array_keys(self::$stomach));
-        return $key !== false ? $key : $fallback;
     }
 
 }
