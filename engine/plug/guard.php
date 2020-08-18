8756f13bbfd480f47f037f4808a4882e1d2e1d89
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-08-18 19:03:51 +0700

    Test

--- engine/plug/guard.php
+++ engine/plug/guard.php
@@ -2,6 +2,6 @@
 
 Guard::_('kick', function(string $path = null) {
     $path = $path ?? $GLOBALS['url']->current;
-    header('Location: ' . URL::long($path, false));
+    header('location: ' . URL::long($path, false));
     exit;
 });
