668945b92c26388ce9f69451de1b55da992c94d8
Taufik Nurrohman <hi@taufik-nurrohman.com>
2021-12-13 09:18:56 +0700

    Move Layout File

--- lot/x/alert/index.php
+++ lot/x/alert/index.php
@@ -1,5 +1,5 @@
 <?php
 
 if (!is_file(LOT . D . 'layout' . D . 'alert.php')) {
-    Layout::set('alert', __DIR__ . D . 'engine' . D . 'r' . D . 'layout.php');
+    Layout::set('alert', __DIR__ . D . 'lot' . D . 'layout' . D . 'alert.php');
 }
