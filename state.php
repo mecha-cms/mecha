9dc83d68112c83d6fc3e7f99ed238c2bf928a163
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-03-29 13:17:16 +0700

    Resolve #207

--- state.php
+++ state.php
@@ -1,9 +1,7 @@
 <?php
 
 return [
-    'charset' => 'utf-8',
     'description' => 'Site description.',
-    'direction' => 'ltr',
     'language' => 'en',
     'route' => '/index',
     'title' => 'Site Title',
