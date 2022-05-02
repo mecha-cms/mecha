82b192e903b2136c6c9fa8059c51812e6cc568e5
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-05-03 05:30:52 +0700

    Bug Fix

--- engine/kernel/time.php
+++ engine/kernel/time.php
@@ -17,7 +17,11 @@ final class Time extends Genome {
         if (is_numeric($value)) {
             $this->source = date('Y-m-d H:i:s', $value);
         } else if (strlen($value) >= 19 && 5 === substr_count($value, '-')) {
-            $this->source = \DateTime::createFromFormat('Y-m-d-H-i-s', $value)->format('Y-m-d H:i:s');
+            if ($date = \DateTime::createFromFormat('Y-m-d-H-i-s', $value)) {
+                $this->source = $date->format('Y-m-d H:i:s');
+            } else {
+                $this->source = date('Y-m-d H:i:s', 0);
+            }
         } else {
             $this->source = date('Y-m-d H:i:s', strtotime($value));
         }
