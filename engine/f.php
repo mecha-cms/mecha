4215af44fdc8d3f60dcf5fabfc2935619dfd329d
Taufik Nurrohman <t.nurrohman77@gmail.com>
2022-09-08 00:35:15 +0700

    Must Be Called Immediately :(

--- engine/f.php
+++ engine/f.php
@@ -1,5 +1,43 @@
 <?php
 
+// This feature is available since PHP 8.1
+if (!function_exists('array_is_list')) {
+    $keep = true;
+    function array_is_list(array $value) {
+        if ([] === $value) {
+            return true;
+        }
+        $key = -1;
+        foreach ($value as $k => $v) {
+            if ($k !== ++$key) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// This feature is available since PHP 7.3
+if (!function_exists('array_key_first')) {
+    $keep = true;
+    function array_key_first(array $value) {
+        if ($value) {
+            foreach ($value as $k => $v) {
+                return $k;
+            }
+        }
+        return null;
+    }
+}
+
+// This feature is available since PHP 7.3
+if (!function_exists('array_key_last')) {
+    $keep = true;
+    function array_key_last(array $value) {
+        return $value ? key(array_slice($value, -1, 1, true)) : null;
+    }
+}
+
 function abort(string $alert, $exit = true) {
     ob_start();
     debug_print_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
