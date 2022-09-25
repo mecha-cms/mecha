de0ea8f11526c91a8c1401666db893760586b415
Taufik Nurrohman <t.nurrohman77@gmail.com>
2022-09-25 21:52:06 +0700

    Update

--- engine/kernel/h-t-m-l.php
+++ engine/kernel/h-t-m-l.php
@@ -8,18 +8,21 @@ class HTML extends XML {
         'textarea' => 1
     ];
 
-    // <https://html.spec.whatwg.org/dev/syntax.html#void-elements>
+    // <https://www.w3.org/TR/2011/WD-html-markup-20110113/syntax.html#void-element>
     protected $void = [
         'area' => 1,
         'base' => 1,
         'br' => 1,
         'col' => 1,
+        'command' => 1,
         'embed' => 1,
         'hr' => 1,
         'img' => 1,
         'input' => 1,
+        'keygen' => 1,
         'link' => 1,
         'meta' => 1,
+        'param' => 1,
         'source' => 1,
         'track' => 1,
         'wbr' => 1
@@ -28,8 +31,8 @@ class HTML extends XML {
     public $strict = false;
 
     public function __construct($value = [], $deep = false) {
-        if (is_string($value) && $deep) {
-            // Convert `<br>` to `<br/>`
+        if ($deep && is_string($value) && !$this->strict) {
+            // Convert `<tag>` to `<tag/>`
             $value = preg_replace('/<(' . implode('|', array_keys(array_filter($this->void))) . ')(\s(?:"[^"]*"|\'[^\']*\'|[^\/>])*)?>/i', '<$1$2/>', $value);
         }
         parent::__construct($value, $deep);
