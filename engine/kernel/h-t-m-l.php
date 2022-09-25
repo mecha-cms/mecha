d32bf7c82dfdd0980cd8a4f42f439f94df936297
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-09-25 13:04:54 +0700

    Fix HTML5 Void Element(s) in Deep Mode

--- engine/kernel/h-t-m-l.php
+++ engine/kernel/h-t-m-l.php
@@ -8,6 +8,31 @@ class HTML extends XML {
         'textarea' => 1
     ];
 
+    // <https://html.spec.whatwg.org/dev/syntax.html#void-elements>
+    protected $void = [
+        'area' => 1,
+        'base' => 1,
+        'br' => 1,
+        'col' => 1,
+        'embed' => 1,
+        'hr' => 1,
+        'img' => 1,
+        'input' => 1,
+        'link' => 1,
+        'meta' => 1,
+        'source' => 1,
+        'track' => 1,
+        'wbr' => 1
+    ];
+
     public $strict = false;
+
+    public function __construct($value = [], $deep = false) {
+        if (is_string($value) && $deep) {
+            // Convert `<br>` to `<br/>`
+            $value = preg_replace('/<(' . implode('|', array_keys(array_filter($this->void))) . ')(\s(?:"[^"]*"|\'[^\']*\'|[^\/>])*)?>/i', '<$1$2/>', $value);
+        }
+        parent::__construct($value, $deep);
+    }
 
 }
