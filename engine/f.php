8382b3dddb7c4b2052dd59155933c1af052f03f4
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-02-22 23:51:46 +0700

    Update

--- engine/f.php
+++ engine/f.php
@@ -1227,12 +1227,12 @@ function w(?string $value, $keep = [], $break = false) {
     // Should be a HTML input (if `$keep` is not empty, assume that input is a HTML anyway)
     if ($keep || false !== strpos($value, '<') || false !== strpos($value, ' ') || false !== strpos($value, "\n")) {
         $keep = is_string($keep) ? explode(',', $keep) : (array) $keep;
-        $value = preg_replace($break ? '/ +/' : '/\s+/', ' ', trim(strip_tags($value, $keep)));
+        $value = trim(preg_replace($break ? '/ +/' : '/\s+/', ' ', strip_tags($value, $keep)));
         return "" !== $value ? $value : null;
     }
     // Remove `-`, `.`, `_`, and `~` prefix/suffix from the file name then replace `-` with a space. If you want to keep
     // the `-`, be sure to write it as `%2D` in the file name. Then, it decodes the file name as if it were an URL.
-    $value = rawurldecode(preg_replace('/[-]+/', ' ', trim($value, '-._~')));
+    $value = trim(rawurldecode(preg_replace('/[-]+/', ' ', trim($value, '-._~'))));
     return "" !== $value ? $value : null;
 }
 
