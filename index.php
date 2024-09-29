e30388be85a96e3a804bfc8adeebf456fc4028d3
Taufik Nurrohman <hi@taufik-nurrohman.com>
2024-09-29 13:16:21 +0700

    Update Version

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
  * -----------------------------------------
  */
 
-define('VERSION', '3.0.1'); // Current version
+define('VERSION', '3.0.2'); // Current version
 
 define('D', DIRECTORY_SEPARATOR); // Directory separator
 define('N', PHP_EOL); // Line break
